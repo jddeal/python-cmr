f35cac6890fbe7a0ef21e04d89360e7fe5909a76
misnor <N86879509>
2017-01-14 00:37:24 -0400

    Basic Granule Query + Tests

---
+++ tests/test_granule.py
@@ -0,0 +1,37 @@
+import unittest
+from pycmr.granule import GranuleQuery
+
+class TestGranuleClass(unittest.TestCase):
+
+    short_name_val = "MOD09GA"
+    short_name = "short_name"
+
+    version_val = "006"
+    version = "version"
+
+    point_val = "44.6,-63.6"
+    point = "point"
+
+    def test_short_name(self):
+        query = GranuleQuery()
+        query.short_name(self.short_name_val)
+
+        self.assertIn(self.short_name, query.params)
+        self.assertEqual(query.params[self.short_name], self.short_name_val)
+
+    def test_version(self):
+        query = GranuleQuery()
+        query.version(self.version_val)
+
+        self.assertIn(self.version, query.params)
+        self.assertEqual(query.params[self.version], self.version_val)
+
+    def test_point(self):
+        query = GranuleQuery()
+        query.point(self.point_val)
+
+        self.assertIn(self.point, query.params)
+        self.assertEqual(query.params[self.point], self.point_val)
+
+if __name__ == '__main__':
+    unittest()
