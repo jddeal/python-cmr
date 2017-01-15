7ab028ae89fe5fe9452d927c66b2ad0cb98faf25
misnor <N86879509>
2017-01-15 19:55:11 -0400

    #10 + Tests. Added: online_only, downloadable, day_night_flag, orbit_number, entry_title

--- pycmr/granule.py
+++ pycmr/granule.py
@@ -2,7 +2,9 @@
 Module for anything related to Granule searching
 """
 
+import urllib
 from requests import get
+
 
 class GranuleQuery(object):
     """
@@ -10,10 +12,9 @@ class GranuleQuery(object):
     """
 
     base_url = "https://cmr.earthdata.nasa.gov/search/granules.json"
-    params = {}
 
     def __init__(self):
-        pass
+        self.params = {}
 
     def short_name(self, short_name=None):
         """
@@ -49,6 +50,64 @@ class GranuleQuery(object):
         self.params['point'] = point
 
         return self
+
+    def downloadable(self, downloadable):
+        """
+        Set the downloadable value for the query.
+
+        Must be of type Boolean
+        """
+        if not isinstance(downloadable, bool):
+            return
+
+        self.params['downloadable'] = downloadable
+
+    def online_only(self, online_only):
+        """
+        Set the online_only value for the query.
+
+        Must be of type Boolean
+        """
+
+        if not isinstance(online_only, bool):
+            return
+
+        self.params['online_only'] = online_only
+
+    def entry_title(self, entry_title):
+        """
+        Set the entry_title value for the query
+        """
+
+        entry_title = urllib.parse.quote(entry_title)
+
+        self.params['entry_title'] = entry_title
+
+    def orbit_number(self, orbit_number):
+        """"
+        Set the orbit_number value for the query
+        """
+
+        if isinstance(orbit_number, int):
+            self.params['orbit_number'] = orbit_number
+        else:
+            orbit_number = urllib.parse.quote(orbit_number)
+            self.params['orbit_number'] = orbit_number
+
+    def day_night_flag(self, day_night_flag):
+        """
+        Set the day_night_flag value for the query
+        """
+
+        if not isinstance(day_night_flag, str):
+            return
+
+        day_night_flag = day_night_flag.lower()
+
+        if day_night_flag not in ['day', 'night', 'unspecified']:
+            return
+
+        self.params['day_night_flag'] = day_night_flag
 
     def execute(self):
         """
--- tests/test_granule.py
+++ tests/test_granule.py
@@ -12,6 +12,12 @@ class TestGranuleClass(unittest.TestCase):
     point_val = "44.6,-63.6"
     point = "point"
 
+    online_only = "online_only"
+    downloadable = "downloadable"
+    entry_id = "entry_title"
+    orbit_number = "orbit_number"
+    day_night_flag = "day_night_flag"
+
     def test_short_name(self):
         query = GranuleQuery()
         query.short_name(self.short_name_val)
@@ -32,6 +38,86 @@ class TestGranuleClass(unittest.TestCase):
 
         self.assertIn(self.point, query.params)
         self.assertEqual(query.params[self.point], self.point_val)
+
+    def test_online_only_set(self):
+        query = GranuleQuery()
+        query.online_only(True)
+
+        self.assertIn(self.online_only, query.params)
+        self.assertEqual(query.params[self.online_only], True)
+
+    def test_online_only_invalid(self):
+        query = GranuleQuery()
+        query.online_only("Invalid Type")
+
+        self.assertNotIn(self.online_only, query.params)
+
+    def test_downloadable_set(self):
+        query = GranuleQuery()
+        query.downloadable(True)
+
+        self.assertIn(self.downloadable, query.params)
+        self.assertEqual(query.params[self.downloadable], True)
+
+    def test_downloadable_invalid(self):
+        query = GranuleQuery()
+        query.downloadable("Invalid Type")
+
+        self.assertNotIn(self.downloadable, query.params)
+
+    def test_entry_title_set(self):
+        query = GranuleQuery()
+        query.entry_title("DatasetId 5")
+
+        self.assertIn(self.entry_id, query.params)
+        self.assertEqual(query.params[self.entry_id], "DatasetId%205")
+
+    def test_orbit_number_set(self):
+        query = GranuleQuery()
+        query.orbit_number(985)
+
+        self.assertIn(self.orbit_number, query.params)
+        self.assertEqual(query.params[self.orbit_number], 985)
+
+    def test_orbit_number_encode(self):
+        query = GranuleQuery()
+        query.orbit_number("985,986")
+
+        self.assertIn(self.orbit_number, query.params)
+        self.assertEqual(query.params[self.orbit_number],"985%2C986")
+
+    def test_day_night_flag_day_set(self):
+        query = GranuleQuery()
+        query.day_night_flag('day')
+
+        self.assertIn(self.day_night_flag, query.params)
+        self.assertEqual(query.params[self.day_night_flag], 'day')
+
+    def test_day_night_flag_night_set(self):
+        query = GranuleQuery()
+        query.day_night_flag('night')
+
+        self.assertIn(self.day_night_flag, query.params)
+        self.assertEqual(query.params[self.day_night_flag], 'night')
+
+    def test_day_night_flag_unspecified_set(self):
+        query = GranuleQuery()
+        query.day_night_flag('unspecified')
+
+        self.assertIn(self.day_night_flag, query.params)
+        self.assertEqual(query.params[self.day_night_flag], 'unspecified')
+
+    def test_day_night_flag_invalid_set(self):
+        query = GranuleQuery()
+        query.day_night_flag('invaliddaynight')
+
+        self.assertNotIn(self.day_night_flag, query.params)
+
+    def test_day_night_flag_invalid_type_set(self):
+        query = GranuleQuery()
+        query.day_night_flag(True)
+
+        self.assertNotIn(self.day_night_flag, query.params)
 
 if __name__ == '__main__':
     unittest()
