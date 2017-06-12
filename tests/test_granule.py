1a2f544d6c0c73b96cc008a535f1f59a7cedd954
Justin <deal.justin@gmail.com>
2017-06-11 19:36:04 -0500

    Add params shortcut method (#53)
    * Added a parameters() method that allows for passing all parameters as kwargs
    
    * Added tests for parameters method

--- tests/test_granule.py
+++ tests/test_granule.py
@@ -350,6 +350,22 @@ class TestGranuleClass(unittest.TestCase):
         with self.assertRaises(ValueError):
             query = GranuleQuery(None)
     
+    def test_valid_parameters(self):
+        query = GranuleQuery()
+
+        query.parameters(short_name="AST_L1T", version="003", point=(-100, 42))
+
+        self.assertEqual(query.params["short_name"], "AST_L1T")
+        self.assertEqual(query.params["version"], "003")
+        self.assertEqual(query.params["point"], "-100.0,42.0")
+    
+    def test_invalid_parameters(self):
+        query = GranuleQuery()
+
+        with self.assertRaises(ValueError):
+            query.parameters(fake=123)
+            query.parameters(point=(-100, "badvalue"))
+
     def test_valid_formats(self):
         query = GranuleQuery()
         formats = ["json", "xml", "echo10", "iso", "iso19115", "csv", "atom", "kml", "native"]
